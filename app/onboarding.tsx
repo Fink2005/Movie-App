a42240763fed47cde8269fa457cb92d3b69f5849
Fink2005 <phans3806@gmail.com>
2025-09-05 04:34:13 +0700

    feat: init

---
+++ app/onboarding.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const onboarding = () => {
+  return (
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-2xl font-bold text-green-500'>onboarding</Text>
+    </View>
+  )
+}
+
+export default onboarding
---
+++ components/SearchBar.tsx
@@ -0,0 +1,17 @@
+import { icons } from '@/constants/icons';
+import React from 'react';
+import { Image, TextInput, View } from 'react-native';
+interface Props {
+    onPress: () => void;
+    placeholder: string;
+}
+const SearchBar = ({onPress, placeholder}: Props) => {
+  return (
+    <View className='flex-row items-center px-5 py-4 rounded-full bg-dark-200'>
+        <Image source={icons.search} className='size-5' resizeMode='contain' tintColor="#ab8bff"/>
+        <TextInput className='flex-1 ml-2 text-white' placeholder={placeholder} placeholderTextColor="#a8b5db" onPress={onPress} />
+    </View>
+  )
+}
+
+export default SearchBar
